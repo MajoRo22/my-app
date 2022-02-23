5069c412a40fa21d781201396376ac40719e83e3
Majo <descalsmaria@hotmail.com>
2022-02-23 15:20:31 -0300

    Update App.js
    nuevo agregado de texto

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Prueba de texto </p>
+        <p>prueba 2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
